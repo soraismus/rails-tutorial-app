bd04723ccd17579a03cca71178fc00cf1405abb6
M. H. Declup <tertius_lupus@yahoo.com>
2013-09-09 13:57:19 -0700

    users_controller.rb: added redirect_to @user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     # @user = User.new(params[:user])
     @user = User.new(user_params)
     if @user.save
+      redirect_to @user
     else
       render 'new'
     end
