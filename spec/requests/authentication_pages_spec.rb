8be67a8a499ea4a20ba506cd94deed2d991adcde
M. H. Declup <tertius_lupus@yahoo.com>
2013-09-09 15:29:50 -0700

    authentication_pages_spec.rb: added tests for signing in with both invalid and valid information

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,13 +1,42 @@
 require 'spec_helper'
 
-describe "AuthenticationPages" do
+describe "Authentication" do
 
   subject { page }
+  before { visit signin_path }
 
   describe "sign-in page" do
-    before { visit signin_path }
-
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
   end
+
+  describe "signing in" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      fill_in "Email", with: user.email.upcase
+    end
+
+    describe "with invalid information" do
+      before do
+        fill_in "Password", with: "invalid"
+        click_button "Sign in"
+      end
+
+      it { should have_title("Sign in") }
+      it { should have_selector('div.alert.alert-error') }
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+
+      it { should have_title(user.name) }
+      it { should have_link("Profile", href: user_path(user)) }
+      it { should have_link("Sign out", href: signout_path) }
+      it { should_not have_link("Sign in", href: signin_path) }
+    end
+  end
+
 end
