3987e4356e4a69c2c4f41e622c1b5e72e24bf18a
M. H. Declup <tertius_lupus@yahoo.com>
2013-09-10 14:18:49 -0700

    users_controller.rb: fixed typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
     end
 
     def signed_in_user
-      redirect_to sign_in_url, notice: "Please sign in." unless signed_in?
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
 
 end
