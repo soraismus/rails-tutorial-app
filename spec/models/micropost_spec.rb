20d4529dea0a513d4ff11ff51db390667dcbd58e
M. H. Declup <tertius_lupus@yahoo.com>
2013-09-12 16:48:01 -0700

    micropost_spec.rb: tested for user creation of a micropost

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Micropost do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+  before(:each) do
+    # The following code is not idiomatically correct:
+    # @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
+    # The following code is idiomatically correct:
+    user.microposts.build(content: "Lorem ipsum")
+  end
+
+  subject { @micropost }
+
+  it { should respond_to :content }
+  it { should respond_to :user_id }
 end
