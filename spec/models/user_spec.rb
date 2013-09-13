c897e872f19a8fac3284942dd94ad9269dd3cf94
M. H. Declup <tertius_lupus@yahoo.com>
2013-09-13 15:42:27 -0700

    user_spec.rb: added 3 tests from Hartl listing 11.13

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,8 @@ describe User do
    it { should respond_to :feed }
    it { should respond_to :relationships }
    it { should respond_to :followed_users }
+   it { should respond_to :follow! }
+   it { should respond_to :unfollow! }
 
    it { should be_valid }
    it { should_not be_admin }
@@ -160,6 +162,13 @@ describe User do
 
     it { should be_following(other_user) }
     its(:followed_users) { should include(other_user) }
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
   end
 
 end
