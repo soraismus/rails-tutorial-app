b0ee2984801272457381805a8294806ba1baee59
M. H. Declup <tertius_lupus@yahoo.com>
2013-09-13 10:00:31 -0700

    micropost_spec.rb: fixed typo

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -29,7 +29,7 @@ describe Micropost do
   end
 
   describe "with content that is too long" do
-    before { @microposts.content = "a" * 141 }
+    before { @micropost.content = "a" * 141 }
     it { should_not be_valid }
   end
 end
