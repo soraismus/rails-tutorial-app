3dc984d7ab122b7a6741844c01cc6635be93b10c
M. H. Declup <tertius_lupus@yahoo.com>
2013-09-14 12:53:41 -0700

    authentication_pages_spec.rb: fixed typo: 'relationship_path(1)' rather than 'relationships_path(1)'

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -115,8 +115,8 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
 
-        describe "submitting to the create action" do
-          before { delete relationships_path(1) }
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
           specify { expect(response).to redirect_to(signin_path) }
         end
       end
