448ab00b1a5aa81a415f0a75ba49e5ffebc874c9
M. H. Declup <tertius_lupus@yahoo.com>
2013-09-27 17:34:23 -0400

    channel_spec.rb: added 5 specs

--- spec/models/channel_spec.rb
+++ spec/models/channel_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Channel do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+  let(:channel) do
+    user.channels.build(name: "channel_name", open: true)
+  end
+
+  subject { channel }
+
+  it { should be_valid }
+
+  it { should respond_to :name }
+  it { should respond_to :open }
+  it { should respont_to :user_id }
+  it { should respond_to :user }
 end
