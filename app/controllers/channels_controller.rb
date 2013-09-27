67fdafc8bf4a9677fa507f08ff04f681aa9ab622
M. H. Declup <tertius_lupus@yahoo.com>
2013-09-27 19:50:18 -0400

    channels_controller.rb: added before_action, defined #create action, added empty #destroy action

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,2 +1,15 @@
 class ChannelsController < ApplicationController
+  before_action :signed_in_user
+
+  def create
+    cuid = current_user.id
+    contact = User.find_by_name(params[:chat][:contact])
+    channel_name = "cxn-#{cuid}-#{contact.id}"
+    @cu_channel = current_user.channels.build(name: channel_name)
+    @contact_channel = contact.channels.build(name: channel_name)
+  end
+
+  def destroy
+  end
+
 end
