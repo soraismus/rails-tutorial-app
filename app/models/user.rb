868357c3d7ef8bac098938ef3db1c465e02e6f77
M. H. Declup <tertius_lupus@yahoo.com>
2013-09-27 18:26:52 -0400

    user.rb: added channel association, after_create callback, and #create_infrastructure_channel method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,14 @@ class User < ActiveRecord::Base
                                    class_name: "Relationship",
                                    dependent: :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
+
+  has_many :channels, dependent: :destroy
+
   before_create :create_remember_token
   before_save { self.email = email.downcase }
+
+  after_create :create_infrastructure_channel
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
@@ -44,5 +50,10 @@ class User < ActiveRecord::Base
 
     def create_remember_token
       self.remember_token = User.encrypt(User.new_remember_token)
+    end
+
+    def create_infrastructure_channel
+      name = "infrastructure-#{self.id}"
+      self.channels.build(name: name, open: true).save
     end
 end
