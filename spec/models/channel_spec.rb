5a3c4c389d1b477d60217ef3cd61df25983df21a
M. H. Declup <tertius_lupus@yahoo.com>
2013-09-27 19:24:32 -0400

    channel_spec.rb: added 2 specs

--- spec/models/channel_spec.rb
+++ spec/models/channel_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
+create_10_users = lambda do
+  10.times do |i|
+    input = {}
+    input[:name] = "user#{i}"
+    input[:email] = "a#{i}@example.com"
+    input[:password] = "password"
+    input[:password_confirmation] = "password"
+    User.create(input)
+  end
+end
+
+destroy_10_users = lambda do
+  10.times do |i|
+    User.find_by_name("user#{i}").destroy
+  end
+end
+
 describe Channel do
   let(:user) { FactoryGirl.create(:user) }
   let(:channel) do
@@ -14,4 +31,18 @@ describe Channel do
   it { should respond_to :open }
   it { should respond_to :user_id }
   it { should respond_to :user }
+
+  describe "when 10 users are created and then deleted" do
+    specify "10 channels should be created concurrently" do
+      expect { create_10_users.call }.to change(Channel, :count).by(10)
+    end
+  end
+
+  describe "when 10 users are deleted" do
+    before { create_10_users.call }
+    specify "at least 10 channels should be destroyed concurrently" do
+      expect { destroy_10_users.call }.to change(Channel, :count).by(-10)
+    end
+  end
+
 end
