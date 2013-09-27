341397a5cb684ef04ec4ad5dd882362436f71fd1
M. H. Declup <tertius_lupus@yahoo.com>
2013-09-27 17:54:33 -0400

    schema.rb: cli: rake db:drop & rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130913215208) do
+ActiveRecord::Schema.define(version: 20130927212844) do
+
+  create_table "channels", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.boolean  "open"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "channels", ["name"], name: "index_channels_on_name", unique: true
+  add_index "channels", ["user_id"], name: "index_channels_on_user_id"
 
   create_table "microposts", force: true do |t|
     t.string   "content"
