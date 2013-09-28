5114a9f101a966fb9183407042c1c0405e3e207b
M. H. Declup <tertius_lupus@yahoo.com>
2013-09-28 00:26:19 -0400

    channels_controller.rb: save channels and redirect_to 'static_pages#chat'

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -5,8 +5,9 @@ class ChannelsController < ApplicationController
     cuid = current_user.id
     contact = User.find_by_name(params[:chat][:contact])
     channel_name = "cxn-#{cuid}-#{contact.id}"
-    @cu_channel = current_user.channels.build(name: channel_name)
-    @contact_channel = contact.channels.build(name: channel_name)
+    @cu_channel = current_user.channels.build(name: channel_name).save!
+    @contact_channel = contact.channels.build(name: channel_name).save!
+    redirect_to "static_pages#chat"
   end
 
   def destroy
