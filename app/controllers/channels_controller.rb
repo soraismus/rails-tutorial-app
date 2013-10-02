e8c9ff0378eb6719f272c5b95ca2514dc5d8783d
M. H. Declup <tertius_lupus@yahoo.com>
2013-10-02 14:28:53 -0400

    channels_controller.rb: reverted to previous state to test Heroku deployment

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,40 +2,23 @@ class ChannelsController < ApplicationController
   before_action :signed_in_user
 
   def create
+    cuid = current_user.id
     contact = User.find_by_name(params[:chat][:contact])
-    channel_name = "/cxn-#{current_user.id}-#{contact.id}"
-    endpoints = [current_user, contact]
+    channel_name = "/cxn-#{cuid}-#{contact.id}"
+    @cu_channel = current_user.channels.build(name: channel_name)
+    @cu_channel.save!
 
-    # ActiveModel::Base.transaction do
-    endpoints.each do |user|
-      build_connection(user, channel_name)
-    end
+    uri = URI.parse(Rails.application.config.faye_url)
+    message = {
+      channel: "/infrastructure-#{cuid}",
+      data: channel_name
+    }
+    Net::HTTP.post_form(uri, :message => message.to_json)
 
     render nothing: true
   end
 
   def destroy
-    channels = Channel.where(name: params[:channel_name])
-    channels.each { |ch| ch.destroy }
   end
-
-  private
-
-    def build_connection(user, channel_name)
-      user.channels.build(name: channel_name).save!
-      message = msg(user, channel_name)
-      Net::HTTP.post_form(uri, message: message)
-    end
-
-    def msg(user, channel_name)
-      msg = {}
-      msg[:channel] = "/infrastructure-#{user.id}"
-      msg[:data] = channel_name
-      msg.to_json
-    end
-
-    def uri
-      @uri ||= URI.parse(Rails.application.config.faye_url)
-    end
 
 end
