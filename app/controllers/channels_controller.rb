8270994cae972d26b963bb95d2791c59c12bf0ad
M. H. Declup <tertius_lupus@yahoo.com>
2013-10-08 19:29:02 -0400

    channels_controller.rb: render json and allow client to do work

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,20 +2,12 @@ class ChannelsController < ApplicationController
   before_action :signed_in_user
 
   def create
-    cuid = current_user.id
     contact = User.find_by_name(params[:chat][:contact])
-    channel_name = "/cxn-#{cuid}-#{contact.id}"
-    @cu_channel = current_user.channels.build(name: channel_name)
-    @cu_channel.save!
-
-    uri = URI.parse(Rails.application.config.faye_url)
-    message = {
-      channel: "/infrastructure-#{cuid}",
-      data: channel_name
+    data = {
+      cuid: current_user.id,
+      contactId: contact.id
     }
-    Net::HTTP.post_form(uri, :message => message.to_json)
-
-    render nothing: true
+    render json: data
   end
 
   def destroy
